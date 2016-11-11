9aa6cbcd1ae8ad6f56f08f67f0be646ca349c2aa
caleblareau <caleblareau@g.harvard.edu>
2016-11-11 10:16:14 -0500

    color fixing with new plotly version

--- server.R
+++ server.R
@@ -6,16 +6,16 @@ function(input, output, session) {
   output$plot1 <- renderPlotly({
     colr <- as.character(df[,22])
     name  = as.character(df[,1])
-    
     key <- row.names(df)
+    cf <- factor(colr, levels = as.character(unique(colr)), ordered = TRUE)
+    
     dfp <- data.frame(x     = df[,input$xcol],  
                       y     = df[,input$ycol],  
                       z     = df[,input$zcol],
-                      name  = as.character(df[,1]))
-    cf <- factor(colr, levels = as.character(unique(colr)), ordered = TRUE)
-    dfp$col <- as.integer(cf)
+                      name  = as.character(df[,1]),
+                      colidx = as.integer(cf))
     plot_ly(dfp, x = ~x, y = ~y, z = ~z, type="scatter3d", mode="markers", marker = list(size = 3),
-            color = ~col, colors = rev(as.character(unique(cf))), text = c(paste0("Name: ", name, "<br>")), showscale=FALSE, showlegend = FALSE)
+            color = ~as.ordered(colidx), colors = as.character(unique(cf)), text = c(paste0("Name: ", name, "<br>")), showscale=FALSE, showlegend = FALSE)
   })
   
 }
